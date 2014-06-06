fe946b36d7c3720ac24866b868e1fd3adfdcd401
Andrew Steinmeyer <jandrewsteinmeyer@gmail.com>
2014-06-06 14:29:22 -0400

    first commit of blueneko project to repo

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'capistrano', '~> 3.2.1'
+gem 'capistrano-bundler'
+gem 'capistrano-rbenv', '~>2.0'
+
---
+++ config/deploy/development.rb
@@ -0,0 +1,21 @@
+set :stage, :production
+set :branch, "master"
+set :rsync_location, "vagrant"
+
+# used in case we're deploying multiple versions of the same
+# app side by side. Also provides quick sanity checks when looking
+# at filepaths
+set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
+
+# send to vagrant
+server 'deploy@127.0.0.1:2222', user: 'deploy', roles: %w{web app db}, primary: true
+
+set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
+
+# dont try and infer something as important as environment from
+# stage name.
+set :rails_env, :production
+
+# whether we're using ssl or not, used for building nginx
+# config file
+set :enable_ssl, false
